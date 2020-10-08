53ce637d43cbee73add1ad96313756da3ceb7302
Michael Lesser <mlesser@arizona.edu>
2020-10-08 14:10:30 -0700

    cleanup

--- azcam_status/blueprints_status.py
+++ azcam_status/blueprints_status.py
@@ -1,29 +1,24 @@
 """
-Webobs commands for webobs application.
+Browser-based status application.
 """
-import os
 
-from flask import Blueprint, request, render_template
-from flask.helpers import url_for
-from werkzeug.utils import secure_filename
+from flask import Blueprint, render_template
 
 import azcam
 
+status = Blueprint(
+    "status",
+    __name__,
+    static_folder="static_status",
+    template_folder="",
+)
 
-class StatusBlueprint(object):
-    def __init__(self) -> None:
 
-        status = Blueprint(
-            "status",
-            __name__,
-            static_folder="static_status",
-            template_folder="",
-        )
+@status.route("/status", defaults={"page": "status"}, methods=["GET"])
+def show_status(page):
+    return render_template(f"{page}.html")
 
-        @status.route("/status", defaults={"page": "status"}, methods=["GET"])
-        def show_status(page):
-            return render_template(f"{page}.html")
 
-        self.app.register_blueprint(status)
+azcam.db.webapp.register_blueprint(status)
 
-        azcam.log("Loaded azcam_status")
+azcam.log("Loaded azcam_status")
